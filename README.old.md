9c2ae5d1df2c6a9d30f1490a10e2780a69cdab3a
0xNonCents <0xNonCents@gmail.com>
2022-02-07 17:18:55 -0500

    mock pool view

---
+++ README.old.md
@@ -0,0 +1,5 @@
+# mammoth_ui
+
+UI for mammoth pool
+
+Made with Create React App
---
+++ src/features/pool/pool.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import styles from "./Pool.module.css";
+
+export const Pool = () => {
+  return (
+    <div>
+      <div className={styles.row}>
+        <div className={styles.infoContainer}>
+          <h4>Your Balance</h4>
+          <div className={styles.row}>
+            LP Token : <b>100</b>
+          </div>
+        </div>
+        <div className={styles.infoContainer}>
+          <h4>Pool Balance</h4>
+          <div className={styles.row}>
+            Token One : <b>100</b>
+          </div>
+          <div className={styles.row}>
+            Token Two : <b>50</b>
+          </div>
+          <div className={styles.row}>
+            Token Two : <b>250</b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.row}>
+        <button className={styles.tab}>Deposit</button>
+        <button className={styles.tab}>Withdraw</button>
+        <button className={styles.tab}>Swap</button>
+      </div>
+      <div className={styles.row}>
+        Input
+        <input
+          className={styles.textbox}
+          name="amount"
+          aria-label="Set increment amount"
+          value={100}
+        />
+        Token One for
+        <input
+          className={styles.textbox}
+          name="amount"
+          aria-label="Set increment amount"
+          value={50}
+        />
+        LP Tokens
+        <button className={styles.button}>Deposit</button>
+      </div>
+    </div>
+  );
+};
